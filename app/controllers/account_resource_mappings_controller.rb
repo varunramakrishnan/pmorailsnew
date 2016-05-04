2dd9466732237ca516048bdc17e23e7f4741c31a
vinodkrishnanv <vinodkrishnan_v@listertechnologies.com>
2016-05-04 20:15:48 +0530

    hcases project board

--- app/controllers/account_resource_mappings_controller.rb
+++ app/controllers/account_resource_mappings_controller.rb
@@ -24,8 +24,9 @@ class AccountResourceMappingsController < ApplicationController
     accountresource = AccountResourceMapping.where(account_id: params[:id]).all
     result = []
     accountresource.each do |ser|
-        resname=Resource.find(ser.resource_id).employee_name
-        result << {id: ser.resource_id, employee_name: resname}
+        resname=Resource.find(ser.resource_id)
+        ad=AccountResourceMapping.where(resource_id: ser.resource_id,account_id: params[:id])
+        result << {id: ser.resource_id, resource_id: ser.resource_id, employee_name: resname.employee_name ,account_id: params[:id],name: resname.employee_name , Dates: ad[0][:dates], percentage_loaded: ad[0][:percentage_loaded], minDate: ad[0][:dates][1,ad[0][:dates].length-2].delete(' ').split(",").min.to_i,maxDate: ad[0][:dates][1,ad[0][:dates].length-2].delete(' ').split(",").max.to_i ,saved: 1}
       end
     render json: result  
   end
