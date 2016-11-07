ccec17369e030acb44f64a47b227df3a724c2721
vinodkrishnan <vinodkrishnan_v@listertechnologies.com>
2016-11-07 12:29:27 +0530

    account save fix

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,13 @@
 class Account < ActiveRecord::Base
 	validates :account_code, :presence => true, :uniqueness => true
+  validates :organisational_unit_id, :presence => true
 	belongs_to :organisational_unit
   has_many :account_service_mappings,dependent: :destroy
   has_many :account_resource_mappings,dependent: :destroy
   has_many :projects,dependent: :destroy
   has_many :services, :through => :account_service_mappings
   has_many :resources, :through => :account_resource_mappings
-  before_save :upcase_fields
+  # before_save :upcase_fields
 
    def upcase_fields
       self.account_code.upcase!
